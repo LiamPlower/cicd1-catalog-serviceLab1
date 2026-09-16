bfab9f4f3039698d27f6755509c10eb2c47cf30b
LiamPlower-STUDENT <G00467316@atu.ie>
2026-09-16 14:34:05 +0100

    Added Product to Model package

---
+++ src/main/java/com/example/lab1/model/Products.java
@@ -0,0 +1,16 @@
+package com.example.lab1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Products {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+}
