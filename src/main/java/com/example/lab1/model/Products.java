57eeaed51d4cd9c01810cb691b1540b6269c6da8
LiamPlower-STUDENT <G00467316@atu.ie>
2026-09-23 14:56:14 +0100

    Changed Product to Entity with auto generated id for each row entry, so each row is unique

--- src/main/java/com/example/lab1/model/Products.java
+++ src/main/java/com/example/lab1/model/Products.java
@@ -1,5 +1,9 @@
 package com.example.lab1.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,7 +13,10 @@ import java.math.BigDecimal;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Products {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private BigDecimal price;
